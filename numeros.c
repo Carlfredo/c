5aec3278725dd5569b953e448f0ded98e1da06ac
Carlos Alfredo Andreu Martinez <carandmar3@alu.edu.gva.es>
2024-10-08 13:46:19 +0200

    Add files via upload

---
+++ numeros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main (){
+int n;
+do{
+printf("Introduix un número: ");
+scanf("%d", &n);
+}while((n<1)||(n>5));
+    if(n == 1) printf("U\n");
+    if(n == 2) printf("Dos\n");
+    if(n == 3) printf("Tres\n");
+    if(n == 1) printf("Quatre\n");
+    if(n == 1) printf("Cinc\n");
+    return;
+}
